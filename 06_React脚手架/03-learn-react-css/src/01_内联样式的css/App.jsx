643ad9af54787075756f3744b239ae88c630a120
qinhao <coderqin@foxmail.com>
2024-03-07 17:15:53 +0800

    CSS的方案1：内联样式

---
+++ 06_React脚手架/03-learn-react-css/src/01_内联样式的css/App.jsx
@@ -0,0 +1,27 @@
+import React, { PureComponent } from "react";
+
+export class App extends PureComponent {
+  constructor() {
+    super();
+
+    this.state = {
+      titleSize: 30,
+    };
+  }
+  render() {
+    const { titleSize } = this.state;
+    return (
+      <div>
+        <button
+          onClick={() => this.setState({ titleSize: this.state.titleSize + 2 })}
+        >
+          增加titleSize
+        </button>
+        <h2 style={{ color: "red", fontSize: `${titleSize}px` }}>我是标题</h2>
+        <p style={{ color: "skyblue", fontSize: "20px" }}>我是内容，哈哈哈</p>
+      </div>
+    );
+  }
+}
+
+export default App;
