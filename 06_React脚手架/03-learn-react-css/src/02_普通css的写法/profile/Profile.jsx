9089a0ad1567b38c983d99a81ba5ecf7ba378874
qinhao <coderqin@foxmail.com>
2024-03-07 17:16:24 +0800

    CSS的方案2：普通CSS写法

---
+++ 06_React脚手架/03-learn-react-css/src/02_普通css的写法/profile/Profile.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+import './Profile.css'
+
+export class Profile extends PureComponent {
+  render() {
+    return (
+      <div>
+        <div className="section">Profile Section</div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
