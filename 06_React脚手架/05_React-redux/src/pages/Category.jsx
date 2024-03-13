4a1b172b046b989077013e079beebb3207fcc309
qinhao <coderqin@foxmail.com>
2024-03-13 11:29:43 +0800

    Redux-异步网路请求的操作

--- 06_React脚手架/05_React-redux/src/pages/Category.jsx
+++ 06_React脚手架/05_React-redux/src/pages/Category.jsx
@@ -1,19 +1,21 @@
 import React, { PureComponent } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { connect } from "react-redux";
-import {
-  changeBannersAction,
-  changeRecommendsAction,
-} from "../store/actionCreators";
+import { fetchHomeMultiDataAction } from "../store/actionCreators";
+// import {
+//   changeBannersAction,
+//   changeRecommendsAction,
+// } from "../store/actionCreators";
 
 export class Category extends PureComponent {
   componentDidMount() {
-    axios.get("http://codercba.com:8000/home/multidata").then((res) => {
-      const banners = res.data.data.banner.list;
-      const recommends = res.data.data.recommend.list;
-      this.props.changeBanners(banners);
-      this.props.changeRecommends(recommends);
-    });
+    this.props.fetchHomeMultiData();
+    // axios.get("http://codercba.com:8000/home/multidata").then((res) => {
+    //   const banners = res.data.data.banner.list;
+    //   const recommends = res.data.data.recommend.list;
+    //   this.props.changeBanners(banners);
+    //   this.props.changeRecommends(recommends);
+    // });
   }
   render() {
     return (
@@ -30,11 +32,12 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  changeBanners: (banners) => {
-    dispatch(changeBannersAction(banners));
+  //   changeBanners: (banners) => dispatch(changeBannersAction(banners)),
+  //   changeRecommends: (recommends) =>
+  //     dispatch(changeRecommendsAction(recommends)),
+  fetchHomeMultiData() {
+    dispatch(fetchHomeMultiDataAction());
   },
-  changeRecommends: (recommends) =>
-    dispatch(changeRecommendsAction(recommends)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Category);
--- 06_React脚手架/05_React-redux/src/store/actionCreators.js
+++ 06_React脚手架/05_React-redux/src/store/actionCreators.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "./constants";
+import axios from "axios";
 
 export const addNumberAction = (num) => ({
   type: actionTypes.ADD_NUMBER,
@@ -19,3 +20,17 @@ export const changeRecommendsAction = (recommends) => ({
   type: actionTypes.CHANGE_RECOMMENDS,
   recommends,
 });
+
+export const fetchHomeMultiDataAction = () => {
+  function foo(dispatch, getState) {
+    console.log("foo function execution-----", getState());
+    axios.get("http://codercba.com:8000/home/multidata").then((res) => {
+      const banners = res.data.data.banner.list;
+      const recommends = res.data.data.recommend.list;
+      dispatch(changeBannersAction(banners));
+      dispatch(changeRecommendsAction(recommends));
+    });
+  }
+
+  return foo;
+};
