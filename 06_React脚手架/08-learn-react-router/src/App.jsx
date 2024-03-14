cdb0cabb41796f7e52a973d2f769a16b1fc51103
qinhao <coderqin@foxmail.com>
2024-03-14 17:18:56 +0800

    Link和NavLink的使用

--- 06_React脚手架/08-learn-react-router/src/App.jsx
+++ 06_React脚手架/08-learn-react-router/src/App.jsx
@@ -2,15 +2,45 @@ import React, { PureComponent } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import "./style.css";
 
 export class App extends PureComponent {
   render() {
     return (
       <div className="app">
         <div className="header">
+          header
           <div className="nav">
             <Link to="/home">首页</Link>
             <Link to="/about">关于</Link>
+            {/* <NavLink
+              to="/home"
+              style={({ isActive }) => ({
+                color: isActive ? "red" : "#000",
+              })}
+            >
+              首页
+            </NavLink>
+            <NavLink
+              to="/about"
+              style={({ isActive }) => ({
+                color: isActive ? "red" : "#000",
+              })}
+            >
+              关于
+            </NavLink> */}
+            {/* <NavLink
+              to="/home"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              首页
+            </NavLink>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              关于
+            </NavLink> */}
           </div>
           <hr />
         </div>
