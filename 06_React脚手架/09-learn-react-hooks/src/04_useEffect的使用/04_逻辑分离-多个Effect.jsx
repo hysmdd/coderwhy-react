71d144c132ed34267d98fd0977d602b94a3a345a
qinhao <coderqin@foxmail.com>
2024-03-18 13:44:40 +0800

    Hooks-Effect控制时机-控制回调执行

--- 06_React脚手架/09-learn-react-hooks/src/04_useEffect的使用/04_逻辑分离-多个Effect.jsx
+++ 06_React脚手架/09-learn-react-hooks/src/04_useEffect的使用/04_逻辑分离-多个Effect.jsx
@@ -2,28 +2,24 @@ import React, { memo, useEffect, useState } from "react";
 
 const App = memo(() => {
   const [count, setCount] = useState(339);
-  const [message, setMessage] = useState("Hello World");
-
   useEffect(() => {
     console.log("修改标题");
-  }, [count]);
+  });
   useEffect(() => {
     console.log("监听redux中的数据变化");
-    return () => {};
-  }, []);
+    return () => {
+      console.log("取消监听redux中的数据变化");
+    };
+  });
   useEffect(() => {
     console.log("监听eventBus中的dex事件");
-    return () => {};
-  }, []);
-  useEffect(() => {
-    console.log("发送网络请求，获取服务器数据");
-  }, []);
+    return () => {
+      console.log("取消监听eventBus中的dex事件");
+    };
+  });
   return (
     <div>
       <button onClick={(e) => setCount(count + 1)}>{count}(+1)</button>
-      <button onClick={(e) => setMessage("终会与你同行")}>
-        {message}(修改文本)
-      </button>
     </div>
   );
 });
